dcb2e7c7db314ec0ee58323b8fe64323b00a4f17
igorsbrito <igorsbrito93@gmail.com>
2017-07-07 02:52:52 -0300

    update

--- server.py
+++ server.py
@@ -160,4 +160,4 @@ api.add_resource(LogIn,'/login/<string:username>/<string:password>', endpoint='l
 
 
 #finish program
-application.run(host='0.0.0.0', port=80, debug=True)
+application.run(host='0.0.0.0', debug=True)
